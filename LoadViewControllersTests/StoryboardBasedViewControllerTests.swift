cd4f29d8f192e6558345b9e0befbbc784cf314d7
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-09 23:20:34 +0500

    Add StoryboardBasedViewControllerTests suit
    . Add new test suit 'StoryboardBasedViewControllerTests'
    . Implement zero test to confirm communication between test suit and production code

---
+++ LoadViewControllersTests/StoryboardBasedViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  StoryboardBasedViewControllerTests.swift
+//  LoadViewControllersTests
+//
+//  Created by HAQQQABD on 09/08/2023.
+//
+
+import XCTest
+@testable import LoadViewControllers
+
+final class StoryboardBasedViewControllerTests: XCTestCase {
+    
+    func test_zero(){
+        XCTFail("Tests are not implemented in StoryboardBasedViewControllerTests yet")
+    }
+}
