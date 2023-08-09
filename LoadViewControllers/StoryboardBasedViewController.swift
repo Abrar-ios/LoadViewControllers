1fcbdf4cd3bb044547a1ed418a0b7a1ba202852c
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-09 23:05:30 +0500

    Add StoryboardBasedViewController
    Add new Cocoa Touch class 'StoryboardBasedViewController' subclass of UIViewController

---
+++ LoadViewControllers/StoryboardBasedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StoryboardBasedViewController.swift
+//  LoadViewControllers
+//
+//  Created by HAQQQABD on 09/08/2023.
+//
+
+import UIKit
+
+class StoryboardBasedViewController: UIViewController {
+    @IBOutlet var label:UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
