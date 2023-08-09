e802af0e8fdbba157d1c80e1e8b57448929b9288
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-10 00:08:33 +0500

    Add test_zero
    .To test communication between test suit and production

---
+++ LoadViewControllersTests/XIBBasedViewControllerTest.swift
@@ -0,0 +1,16 @@
+//
+//  XIBBasedViewControllerTest.swift
+//  LoadViewControllersTests
+//
+//  Created by HAQQQABD on 10/08/2023.
+//
+
+import XCTest
+@testable import LoadViewControllers
+
+final class XIBBasedViewControllerTests: XCTestCase {
+    
+    func test_zero(){
+        XCTFail("There is no test implemented yet in XIBBasedViewControllerTests")
+    }
+}
